81d672d0f7f626ee3473aa1654c51fd7c5ce0699
David Jelenc <david.jelenc@fri.uni-lj.si>
2016-10-17 16:41:02 +0200

    Updating the examples

--- src/main/java/isp/secrecy/AgentCommunicationAsymmetricCipher.java
+++ src/main/java/isp/secrecy/AgentCommunicationAsymmetricCipher.java
@@ -1,10 +1,7 @@
 package isp.secrecy;
 
-import javax.crypto.Cipher;
-import javax.xml.bind.DatatypeConverter;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -14,7 +11,7 @@ import java.util.concurrent.LinkedBlockingQueue;
  * EXERCISE B4:
  * An agent communication example. Message Confidentiality is provided using asymmetric
  * cypher algorithm.
- *
+ * <p>
  * IMPORTANT: This is an insecure example. One should never encrypt with a TDF (such as RSA) directly.
  * Such construction is deterministic and many known attacks against it exist.
  * <p/>
@@ -44,7 +41,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class AgentCommunicationAsymmetricCipher {
 
-    public static void main(String[] args) throws NoSuchAlgorithmException {
+    public static void main(String[] args) throws Exception {
         final String encryptionAlg = "RSA";
 
         /**
@@ -57,8 +54,8 @@ public class AgentCommunicationAsymmetricCipher {
          * STEP 2.
          * Setup an insecure communication channel.
          */
-        final BlockingQueue<String> alice2bob = new LinkedBlockingQueue<>();
-        final BlockingQueue<String> bob2alice = new LinkedBlockingQueue<>();
+        final BlockingQueue<byte[]> alice2bob = new LinkedBlockingQueue<>();
+        final BlockingQueue<byte[]> bob2alice = new LinkedBlockingQueue<>();
 
         /**
          * STEP 3.
@@ -67,25 +64,21 @@ public class AgentCommunicationAsymmetricCipher {
          * - encrypts it using Bob's public key
          * - sends it to Bob
          */
-        final Agent alice = new Agent(alice2bob, bob2alice, bobKP.getPublic(), encryptionAlg, null, null) {
+        final Agent alice = new Agent("alice", alice2bob, bob2alice, bobKP.getPublic(), encryptionAlg) {
             @Override
-            public void run() {
-                try {
-                    // STEP 3.1:  Alice creates a message
-                    final String text = "I love you Bob. Kisses, Alice.";
+            public void execute() throws Exception {
+                // STEP 3.1:  Alice creates a message
+                final String text = "I love you Bob. Kisses, Alice.";
 
-                    // TODO STEP 3.2: Alice encrypts the text with selected algorithm using Bob's public key.
-
-
-                    // TODO STEP 3.3: Encode the cipher text into string of hexadecimal numbers
+                // TODO STEP 3.2: Alice encrypts the text with selected algorithm using Bob's public key.
 
 
-                    // TODO STEP 3.4: Alice logs the act of sending the message
+                // TODO STEP 3.3: Encode the cipher text into string of hexadecimal numbers
 
-                    // TODO STEP 3.4: Send the message across the channel
-                } catch (Exception ex) {
-                    System.err.println("[Alice] Exception: " + ex.getMessage());
-                }
+
+                // TODO STEP 3.4: Alice logs the act of sending the message
+
+                // TODO STEP 3.4: Send the message across the channel
             }
         };
 
@@ -95,24 +88,20 @@ public class AgentCommunicationAsymmetricCipher {
          * - waits for a message from Alice
          * - upon receiving it, uses his private key to decrypt it
          */
-        final Agent bob = new Agent(bob2alice, alice2bob, bobKP.getPrivate(), encryptionAlg, null, null) {
+        final Agent bob = new Agent("bob", bob2alice, alice2bob, bobKP.getPrivate(), encryptionAlg) {
             @Override
-            public void run() {
-                try {
-                    // STEP 4.1: Bob receives the message
-                    final String cipherTextHEX = incoming.take();
-                    System.out.println("[Bob]: Received " + cipherTextHEX);
+            public void execute() throws Exception {
+                // STEP 4.1: Bob receives the message
+                final byte[] cipherTextHEX = incoming.take();
+                System.out.println("[Bob]: Received " + cipherTextHEX);
 
-                    // TODO STEP 4.2: Decode the incoming string of HEX literals into a byte array
+                // TODO STEP 4.2: Decode the incoming string of HEX literals into a byte array
 
-                    // TODO STEP 4.3: Bob decrypts the cipher text
+                // TODO STEP 4.3: Bob decrypts the cipher text
 
-                    // TODO STEP 4.3: Bob creates a string from the decrypted byte array
+                // TODO STEP 4.3: Bob creates a string from the decrypted byte array
 
-                    // TODO STEP 4.4: Bob displays the text
-                } catch (Exception ex) {
-                    System.out.println("[Bob]: Exception: " + ex.getLocalizedMessage());
-                }
+                // TODO STEP 4.4: Bob displays the text
             }
         };
 
