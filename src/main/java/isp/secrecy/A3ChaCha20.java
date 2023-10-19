b76accc67809bdbe4e9d7077cd59d44579401b10
Franz Muszarsky <f.muszarsky@outlook.de>
2023-10-19 17:51:32 +0200

    solved

--- src/main/java/isp/secrecy/A3ChaCha20.java
+++ src/main/java/isp/secrecy/A3ChaCha20.java
@@ -3,8 +3,13 @@ package isp.secrecy;
 import fri.isp.Agent;
 import fri.isp.Environment;
 
+import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
+import javax.crypto.spec.ChaCha20ParameterSpec;
+import java.nio.ByteBuffer;
+
 import java.security.Key;
+import java.security.SecureRandom;
 
 /**
  * TASK:
@@ -14,10 +19,14 @@ import java.security.Key;
  * https://docs.oracle.com/en/java/javase/11/docs/api/java.base/javax/crypto/Cipher.html
  */
 public class A3ChaCha20 {
+
+    private static byte[] cipherText;
+          // Class-level variable to hold cipher text
+    
     public static void main(String[] args) throws Exception {
         // STEP 1: Alice and Bob beforehand agree upon a cipher algorithm and a shared secret key
         // This key may be accessed as a global variable by both agents
-        final Key key = KeyGenerator.getInstance("AES").generateKey();
+        final Key key = KeyGenerator.getInstance("ChaCha20").generateKey();
 
         // STEP 2: Setup communication
         final Environment env = new Environment();
@@ -32,13 +41,88 @@ public class A3ChaCha20 {
                  *
                  * Recall, ChaCha2 requires that you specify the nonce and the counter explicitly.
                  */
+
+                final byte[] pt = message.getBytes();
+
+                //create nonce
+
+
+                for (int i = 0; i < 10; i++) {
+                    // Alice sends message
+                    final Cipher encrypt = Cipher.getInstance("ChaCha20");
+
+                    final byte[] nonce = new byte [12];
+                    new SecureRandom().nextBytes(nonce);
+                    
+                    final int counter = i;
+                    byte[] CounterArray = ByteBuffer.allocate(4).putInt(counter).array();
+            
+                    encrypt.init(Cipher.ENCRYPT_MODE, key, new ChaCha20ParameterSpec(nonce, counter));
+
+                    cipherText = encrypt.doFinal(pt);
+
+                    send("bob", cipherText);
+                    send("bob", nonce);
+                    send("bob", CounterArray);
+
+                    //Alice receives message
+                    byte[] cipherTextB = receive("bob");
+                    byte[] nonceB = receive("bob");
+                    byte[] CounterArrayB = receive("bob");
+
+                    int counterB = ByteBuffer.wrap(CounterArrayB).getInt();
+
+                    final Cipher decrypt = Cipher.getInstance("ChaCha20");
+                    decrypt.init(Cipher.DECRYPT_MODE, key, new ChaCha20ParameterSpec(nonceB, counterB));
+                    final byte[] dt1 = decrypt.doFinal(cipherTextB);
+
+                    System.out.println("[MessageB]" + new String(dt1));
+
+                    
+                }
             }
-        });
+    });
 
         env.add(new Agent("bob") {
             @Override
             public void task() throws Exception {
                 // TODO
+                final String message1 = "I love you too";
+                final byte[] pt1 = message1.getBytes();
+                
+                for (int i = 0; i < 10; i++) {
+                    // Bob receives message
+                    byte[] cipherText = receive("alice");
+                    byte[] nonce = receive("alice");
+                    byte[] CounterArray = receive("alice");
+
+                    int counter = ByteBuffer.wrap(CounterArray).getInt();
+
+                    final Cipher decrypt = Cipher.getInstance("ChaCha20");
+                    decrypt.init(Cipher.DECRYPT_MODE, key, new ChaCha20ParameterSpec(nonce, counter));
+                    final byte[] dt = decrypt.doFinal(cipherText);
+
+                    System.out.println("[Message]" + new String(dt));
+
+                    // Bob send message
+                    final Cipher encrypt = Cipher.getInstance("ChaCha20");
+
+                    final byte[] nonceB = new byte [12];
+                    new SecureRandom().nextBytes(nonceB);
+                    
+                    final int counterB = i;
+                    byte[] CounterArrayB = ByteBuffer.allocate(4).putInt(counterB).array();
+            
+                    encrypt.init(Cipher.ENCRYPT_MODE, key, new ChaCha20ParameterSpec(nonceB, counterB));
+
+                    cipherText = encrypt.doFinal(pt1);
+                    
+
+                    send("alice", cipherText);
+                    send("alice", nonceB);
+                    send("alice", CounterArrayB);
+                
+                }
             }
         });
 
--- src/main/java/isp/secrecy/A4ExhaustiveSearch.java
+++ src/main/java/isp/secrecy/A4ExhaustiveSearch.java
@@ -1,4 +1,8 @@
 package isp.secrecy;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  * Implement a brute force key search (exhaustive key search) if you know that the
@@ -14,17 +18,63 @@ package isp.secrecy;
  * <p>
  * To manually specify a key, use the class {@link javax.crypto.spec.SecretKeySpec})
  */
+
+
+
 public class A4ExhaustiveSearch {
     public static void main(String[] args) throws Exception {
         final String message = "I would like to keep this text confidential Bob. Kind regards, Alice.";
         System.out.println("[MESSAGE] " + message);
 
         // TODO
+        byte[] keyData = { 100,70, 20, 0, 0, 0, 0, 0};
+
+        // Create a SecretKeySpec object from the key data
+        SecretKeySpec secretKey = new SecretKeySpec(keyData, "DES");
+
+        final byte[] pt = message.getBytes();
+
+        final Cipher encrypt = Cipher.getInstance("DES/ECB/PKCS5Padding");
+        encrypt.init(Cipher.ENCRYPT_MODE, secretKey);
+        final byte[] cipherText = encrypt.doFinal(pt);
+
+        bruteForceKey(cipherText, message);
     }
 
     public static byte[] bruteForceKey(byte[] ct, String message) throws Exception {
-        // TODO
+        //Task: You know the plain text of the message, as well as the last 5 bytes out of 8 that were used as keydata 
+        // in the secretKeySpec. Write an algoritihm that tries to encrypt the ciphertext using all possible key combinations until the 
+        // ciphertext is encrypted to the known plain text
+         
 
+        for (int byte1 = 0; byte1 < 256; byte1++) {
+            for (int byte2 = 0; byte2 < 256; byte2++) {
+                for (int byte3 = 0; byte3 < 256; byte3++) {
+
+                    byte[] TrialData = { (byte) byte1, (byte) byte2, (byte) byte3, 0, 0, 0, 0, 0 };
+                    SecretKeySpec TrialKey = new SecretKeySpec(TrialData, "DES");
+
+                    final Cipher decrypt = Cipher.getInstance("DES/ECB/PKCS5Padding");
+                    decrypt.init(Cipher.DECRYPT_MODE, TrialKey);
+                    
+                    try {
+                        final byte[] dt = decrypt.doFinal(ct);
+                        final byte[] pt = message.getBytes();
+
+                    if (Arrays.equals(dt, pt)) {
+                        System.out.println("Found key:");
+                        System.out.println(Arrays.toString(TrialData));
+                     // Key found, so we can exit the loop
+                        return(TrialData);
+                    }
+                } catch (javax.crypto.BadPaddingException e) {
+                    // BadPaddingException occurs for incorrect keys; continue with the next key
+                }
+                }
+            }
+        }
+        System.out.println("No key found");
         return null;
-    }
-}
+    
+}}
+
